52e4741e162bc735e43aae292db235fec25cd51a
Jesse De Haan <dehaanjesses@gmail.com>
2025-10-01 14:36:09 -0400

    Generate completions at build time

--- completions.fish
+++
@@ -1,6 +0,0 @@
-set -l commands list-courses list-assessments
-complete -c climark -f
-complete -c climark -n "not __fish_seen_subcommand_from $commands" \
-    -a "$commands"
-
-complete -c climark -n "__fish_seen_subcommand_from list-assessments" -a "(climark list-courses --format=plain)"
---
+++ src/cli.rs
@@ -0,0 +1,31 @@
+#[derive(clap::Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+    #[arg(long, env)]
+    pub crowdmark_session_token: String,
+}
+
+#[derive(clap::ValueEnum, Clone, Default)]
+pub enum OutputFormat {
+    #[default]
+    Pretty,
+    Plain,
+    Json,
+}
+
+#[derive(clap::Subcommand)]
+pub enum Commands {
+    ListCourses {
+        #[arg(short, long, value_enum, default_value_t)]
+        // json: bool,
+        format: OutputFormat,
+    },
+    ListAssessments {
+        #[arg(env = "CLIMARK_DEFAULT_COURSE")]
+        course_id: String,
+        #[arg(short, long)]
+        json: bool,
+    },
+}
