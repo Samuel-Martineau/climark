50d0b5595a12b39d7e89532c59a622d73e07ada5
Jesse De Haan <me@jdeh.ca>
2025-11-25 20:10:49 -0500

    Don't encode/decode PNG

--- src/upload.rs
+++ src/upload.rs
@@ -2,8 +2,7 @@ use crate::error::ClimarkError;
 use crowdmark::Client;
 use hayro::{Pdf, RenderSettings, render};
 use hayro_interpret::InterpreterSettings;
-use image::ImageFormat;
-use std::io::Cursor;
+use jpeg_encoder::{ColorType, Encoder};
 use std::io::{self, Read};
 use std::sync::Arc;
 
@@ -30,14 +29,10 @@ pub async fn upload_assessment(
         .pages()
         .iter()
         .map(|page| {
-            let png = render(page, &interpreter_settings, &render_settings).take_png();
-            let img = image::load_from_memory(&png).expect("PNG Decode Error");
+            let pixmap = render(page, &interpreter_settings, &render_settings);
 
-            let mut jpeg_data = Vec::new();
-            img.write_to(&mut Cursor::new(&mut jpeg_data), ImageFormat::Jpeg)
-                .expect("JPEG Encode Error");
-
-            jpeg_data
+            let rgb = rgba_to_rgb(pixmap.data_as_u8_slice());
+            encode_jpeg(&rgb, pixmap.width(), pixmap.height())
         })
         .enumerate();
 
@@ -49,4 +44,38 @@ pub async fn upload_assessment(
         client.submit_assessment(&csrf, assessment_id).await?;
     }
     Ok(())
+}
+
+fn encode_jpeg(rgb: &[u8], width: u16, height: u16) -> Vec<u8> {
+    let mut jpeg_data = Vec::new();
+    let encoder = Encoder::new(&mut jpeg_data, 85);
+    encoder.encode(rgb, width, height, ColorType::Rgb).unwrap();
+    jpeg_data
+}
+
+fn rgba_to_rgb(pixels: &[u8]) -> Vec<u8> {
+    let mut rgb = Vec::with_capacity(pixels.len() / 4 * 3);
+
+    for chunk in pixels.chunks_exact(4) {
+        let (r, g, b, a) = (
+            f32::from(chunk[0]),
+            f32::from(chunk[1]),
+            f32::from(chunk[2]),
+            f32::from(chunk[3]),
+        );
+
+        if a == 0.0 {
+            rgb.extend_from_slice(&[255, 255, 255]);
+        } else {
+            let alpha = a / 255.0;
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            {
+                rgb.push((r / alpha).clamp(0.0, 255.0) as u8);
+                rgb.push((g / alpha).clamp(0.0, 255.0) as u8);
+                rgb.push((b / alpha).clamp(0.0, 255.0) as u8);
+            }
+        }
+    }
+
+    rgb
 }
